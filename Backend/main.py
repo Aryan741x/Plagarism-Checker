0cf45670d678e05190926ccddb398b853194b4fa
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2025-07-17 00:27:07 +0530

    wsgi added

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from routes import routes
+from flask_cors import CORS
 
 app = Flask(__name__)
+# Change the origin later to the Frontend URL like  https://your-app.vercel.app for security purposes
+
+CORS(app,origins="*")
 app.register_blueprint(routes,url_prefix="")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# Not Needed for production, but useful for local development
+# if __name__ == "__main__":
+#     app.run(debug=True)
